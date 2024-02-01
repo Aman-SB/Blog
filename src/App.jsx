6a75703267cbca6e44ec34008226adbb50c60d38
Aman Bisht <bishtaman60@gmail.com>
2024-02-01 14:32:36 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
             <div className="w-full block">
                 <Header />
                 <main>
-                TODo : <Outlet />
+                <Outlet />
                 </main>
                 <Footer />
             </div>
